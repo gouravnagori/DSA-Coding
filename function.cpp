7749edd6a87585f595ea071c2e34fbae830984bc
gouravnagori <gouravnagori189@gmail.com>
2026-03-22 17:43:40 +0530

    Add functions

---
+++ function.cpp
@@ -0,0 +1,57 @@
+//it is a block of code which runs when it is called.
+// #include<bits/stdc++.h>
+// using namespace std;
+// void print(); //function decleration //and this is called forward decleration in cpp
+// void print(){  
+//     cout<<"Hello World :)"; //function defination
+// }
+// int  main(){
+//     print();   //function call
+//     return 0;
+// }
+
+
+//Parameters in functions
+// #include<bits/stdc++.h>
+// using namespace std;
+// int sum(int a, int b){  // a,b are parameters
+//     int sum = a+b;
+//     return sum;
+// }
+
+// int diff(int a, int b){  // a,b are parameters
+//     int diff = a-b;
+//     return diff;
+// }
+
+// int def(int a, int b=2){  //here b is a default parameter and we assign the default value(2),in the case when we pass only one value in this then at the place of b it will automatically take 2.
+//     int def = a*b;
+//     return def;
+// }
+// int main(){
+//     cout<<"sum is "<<sum(33,2)<<endl;  // 33,2 arguments
+//     cout<<"diff is "<<diff(3,21)<<endl;  // 3,21 arguments
+//     cout<<"Multiple is "<<def(3); 
+
+
+//     return 0;
+// }
+
+
+//WAF to find the product of two numbers
+// #include<bits/stdc++.h>
+// using namespace std;
+// int product(int a,int b){
+//     int product = a*b;
+//     return product;
+// }
+// int main(){
+//     int a,b;
+//     cout<<"\nEnter the value of a : ";
+//     cin>>a;
+//     cout<<"\nEnter the value of b : ";
+//     cin>>b;
+//     cout<<"\nProduct is "<<product(a,b);
+//     return 0;
+// }
+
